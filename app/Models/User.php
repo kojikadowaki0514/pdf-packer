3e7635628c83e0ab815079ff0e3c3e2b8762f384
kojikadowaki0514 <gbsg9405@yahoo.co.jp>
2025-06-17 22:03:23 +0900

    リレーションの設定

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function uploads() {
+        return $this->hasMany(Upload::class);
+    }
 }
