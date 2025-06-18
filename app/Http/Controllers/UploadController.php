342520a2f47d7aa668d1e62f178ce6bbc95b1cfb
kojikadowaki0514 <gbsg9405@yahoo.co.jp>
2025-06-18 16:28:06 +0900

    dashboadのnavigationの変更

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -8,35 +8,5 @@ use Illuminate\Support\Facades\Storage;
 
 class UploadController extends Controller
 {
-
-    public function index() {
-        return view("upload.index");
-    }
-
-    public function store(Request $request) {
-        $request->validate([
-            'upload_file' => 'required|file|mimes:xlsx,xls,csv,txt',
-        ]);
-
-        $file = $request->file('upload_file');
-
-        // storage/app/uploadsに保存をする
-        $path = $file->store('uploads');
-
-        // dd([
-        //     '存在するか' => $request->hasFile('upload_file'),
-        //     '元のファイル名' => $file->getClientOriginalName(),
-        //     '保存先' => $path,
-        //     'フルパス' => storage_path("app/{$path}"),
-        // ]);
-
-        return back()->with('success', 'ファイルをアップロードしました：' . $file->getClientOriginalName());
-    }
-
-    public function list() {
-        // uploadsディレクトリのファイル一覧を取得
-        $files = Storage::files("uploads");
-
-        return view("upload.list", compact("files"));
-    }
+    
 }
